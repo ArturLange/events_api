1b7e5e694e4f2aa248b3e74f8f0ada9cc7051869
Artur H. Lange <ArturLange@poczta.fm>
2019-06-10 13:17:49 +0200

    Add views for reservation

--- events_api.py
+++ events_api.py
@@ -33,5 +33,45 @@ def get_tickets(event_id):
     })
 
 
+@app.route("/events/<int:event_id>/reservations", methods=["POST"])
+def ticket_reservation():
+    return jsonify({
+        'token': 'ca78fd642f5f655a09b67c0b0f34612c'
+    })
+
+
+@app.route("/events/<int:event_id>/reservations/<reservation_token>")
+def get_reservation(event_id, reservation_token):
+    return jsonify({
+        "paid": False,
+        "event": {
+            "name": "Woodstock Festival",
+            "start_time": datetime(1999, 7, 23, 12).isoformat(),
+            "end_time": datetime(1999, 7, 25, 12).isoformat(),
+            "ticket_types": ["VIP"],
+        },
+        "ticket_type": "VIP"
+    })
+
+
+@app.route("/events/<int:event_id>/reservations/<reservation_token>/pay", methods=["POST"])
+def pay_for_reservation(event_id, reservation_token):
+    return jsonify({
+        "paid": True,
+        "event": {
+            "name": "Woodstock Festival",
+            "start_time": datetime(1999, 7, 23, 12).isoformat(),
+            "end_time": datetime(1999, 7, 25, 12).isoformat(),
+            "ticket_types": ["VIP"],
+        },
+        "ticket_type": "VIP"
+    })
+
+
+@app.route("/stats")
+def get_stats():
+    return jsonify({})
+
+
 if __name__ == "__main__":
     app.run()
